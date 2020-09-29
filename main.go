bb48b7b65c7d0e3d507496f386ccc8cdd7a84a6b
Jon Larabee <1088529+segmentation@users.noreply.github.com>
2020-09-29 14:10:21 -0400

    Update main.go
    Ugh. Killed by rate limiting :(

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello Now Public World!"}
+		res := &response{Message: "Hello Will we Escape Rate Limiting Now!"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
